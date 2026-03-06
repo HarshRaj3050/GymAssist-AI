68ca382fd51310fb1753798586eabcbdd2cb723c
HarshRaj3050 <harshraj3060@gmail.com>
2026-03-07 00:35:44 +0530

    navBar and Home page (only design)

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/NavBar'
+import { Routes, Route } from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/home';
+
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
